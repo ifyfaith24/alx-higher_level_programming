463fed728884d9f296a281fa63df407e6b8f52fc
ifyfaith24 <ifyfaith24@gmail.com>
2023-06-06 14:09:19 -0700

    tasks

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-ef uppercase(str):
+def uppercase(str):
     for i in range(len(str)):
         if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
             atoA = ord(str[i]) + (ord('A') - ord('a'))
